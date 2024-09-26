7041724239c3b0dbab8736ac7a92cbb380e02223
whysilon <valencino1306@gmail.com>
2024-09-26 15:56:47 +0800

    initial commit

---
+++ web_ar_jsx_vue/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const message = ref('Hello!')
+        const changeMsg = () => {
+            if (message.value === "Hello") {
+                message.value = "Hey"
+            } else {
+                message.value = "Hello"
+            }
+        }
+        return {
+            message,
+            changeMsg
+        }
+    },
+    methods: {
+      goToAbout() {
+        this.$router.push("/about")
+      }
+    }
+}
+
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <button @click="changeMsg">Change Message</button>
+    <button @click="goToAbout">Go to About</button>
+  </main>
+</template>
